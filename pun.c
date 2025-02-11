874cadffa7eee9bd869a9af34be76dcd883afa70
Ivan Mutavdzic <imutavdz@f1r3s19.codam.nl>
2025-02-11 13:32:06 +0100

    add

---
+++ pun.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pun.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 02:35:51 by imutavdz          #+#    #+#             */
+/*   Updated: 2025/02/11 02:47:04 by imutavdz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> //I/O standard library
+
+int main()
+{
+	printf("To C, or not to C: that is the question.\n");
+	return (0); //return of the value zero to the O.S.
+}
