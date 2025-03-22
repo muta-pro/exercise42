6bd82d52671a1af6bd4b4f5ca60c28acf7c71de1
Ivan Mutavdzic <imutavdz@f0r4s8.codam.nl>
2025-03-22 17:10:21 +0100

    add

--- anagram_1.c
+++ anagram_1.c
@@ -6,13 +6,18 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 17:50:55 by imutavdz          #+#    #+#             */
-/*   Updated: 2025/03/21 18:09:13 by imutavdz         ###   ########.fr       */
+/*   Updated: 2025/03/22 17:09:58 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //write program that takes two strings
 //if its anagram it retusnt 0 , if not returns other value
 //if there is more than 2 arguments rturns other value
+//so we create an array TAB for char ferquency
+//each index is ascii value
+//
 #include <string.h>
+#include <unistd.h>
+
 int is_anagram(char *s1, char *s2);
 int main(int argc, char *argv[])
 {
@@ -20,33 +25,36 @@ int main(int argc, char *argv[])
 	{
 		int result = is_anagram(argv[1], argv[2]);
 		if (result == 0)
-			return (0);
+			write(1, "yes", 3);
 		else
-			return(1);
+			write(1, "no", 2);
 	}
 	else
-		return (1);
+		write(1, "shit", 4);
 }
 int is_anagram(char *s1, char *s2)
 {
 	int tab[127] = {0};
 	int i = 0;
-	int j = 0;
 
-	while (*s1)
+	while (s1[i])
 	{
-		int sum1 = 0;
-		sum1 = sum1 + tab[(*int)s1[i]];
+		tab[(int)s1[i]]++;//access position in the array
+		i++; //increments the character
+		
+	}
+	i = 0;
+	while (s2[i])
+	{
+		tab[(int)s2[i]]--;
 		i++;
 	}
-	while (*s2)
+	i = 0;
+	while (i < 127)//here it checks if all positions are 0 after decrementing
 	{
-		int sum2 = 0;
-		sum2 = sum2 + tab[(*int)s2[j]];
-		j++;
+		if (tab[i] != 0)
+			return (1);
+		i++;
 	}
-	if (sum2 == sum1)
-		return (0);
-	else
-		return (1);
+	return (0);
 }
